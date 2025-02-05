9fa8cdc4f9cc40756b304cf622dff75b75197d7a
chekurthamahathi21 <mahathireddy2102@gmail.com>
2025-02-05 12:47:49 +0530

    Add files via upload

---
+++ Demo062.java
@@ -0,0 +1,14 @@
+public class Demo062 {
+  public static void main(String[] args) {
+             int[] array = {10, 20, 30, 40, 50}; // Example array
+     
+             // Check if the array has enough elements
+             if (array.length >= 2) {
+                 System.out.println("First element: " + array[0]);
+                 System.out.println("Second element: " + array[1]);
+                 System.out.println("Last element: " + array[array.length - 1]);
+             } else {
+                 System.out.println("The array does not have enough elements.");
+             }
+         }
+     }
